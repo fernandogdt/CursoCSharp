bf8807385c595b1f59175c682ab5eadc42079e13
Elton Fernando <fernandogdt@gmail.com>
2021-10-02 23:17:31 -0300

    Exercicios Secao 06 - 80 e 81

---
+++ repos/CursoC#/Exe06_80/Exe06_80/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Exe06_80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int neg = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+            //    int [] linha = int.Parse(Console.ReadLine().Split(" "));
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] linha = Console.ReadLine().Split(" ");
+                for (int j = 0; j< n; j++)
+                {
+                    matriz[i,j] = int.Parse(linha[j]);
+                }
+
+            }
+            /*
+            foreach (int x in matriz)
+            {
+                Console.WriteLine(x);
+            }
+            */
+
+            Console.WriteLine("Main diagonal:");
+            for (int i = 0;i < n;i++)
+            {
+               for (int j = 0; j < n; j++)
+                {
+                    if ( i == j)
+                        Console.Write(matriz[i,j] + " ");
+
+                    if (matriz[i, j] < 0)
+                        neg++;
+                }
+
+            }
+
+            Console.WriteLine();
+             Console.Write("Negative numbers = " + neg);
+            // Console.WriteLine(matriz.Rank);
+
+            //for (int i = 0; i < matriz.)
+
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ repos/CursoC#/Exe06_81/Exe06_81/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exe06_81
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] z = Console.ReadLine().Split(' ');
+            int x = int.Parse(z[0]);
+            int y = int.Parse(z[1]);
+
+            int[,] mat = new int[x,y];
+            string[] linha;
+          //  Console.WriteLine(mat.GetLength(0));
+          //  Console.WriteLine(mat.GetLength(1));
+
+            for (int i = 0; i < x; i++)
+            {
+                 linha = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < y; j++)
+                {
+                    mat[i, j] = int.Parse(linha[j]);
+                }
+            }
+            
+            int h = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                   
+                    if (h == mat[i, j])
+                    {
+                        Console.WriteLine("Position {0},{1}:",i,j );
+
+                        if (j > 0)
+                           Console.WriteLine("Left: " + mat[i,j-1]);
+                        if (j < mat.GetLength(1)-1)
+                            Console.WriteLine($"Right: {mat[i,j+1]}" );
+                        if (i > 0)
+                            Console.WriteLine("Up: " + mat[i-1,j] );
+                        if (i < mat.GetLength(0) - 1){
+                            Console.WriteLine("Down: " + mat[i+1,j]);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
